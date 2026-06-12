486f855582852426570347dbfca5ce9996aa56ab
Neha Bommireddy <nehabommireddy14@gmail.com>
2026-06-12 18:12:52 +0530

    Changed

--- BackEnd/shoplite/src/main/java/com/ecommerce/shoplite/config/SecurityConfig.java
+++ BackEnd/shoplite/src/main/java/com/ecommerce/shoplite/config/SecurityConfig.java
@@ -148,8 +148,11 @@ public class SecurityConfig {
 
                 org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
 
-                config.setAllowedOriginPatterns(
-                                java.util.List.of("*"));
+                config.setAllowedOrigins(
+                                java.util.List.of(
+                                                "http://13.204.61.123",
+                                                "http://localhost:5173",
+                                                "http://localhost:3000"));
 
                 config.setAllowedMethods(
                                 java.util.List.of(
